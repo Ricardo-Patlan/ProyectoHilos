2fdc8862c3b91fbfcd09da1ff802c138f9fe51ff
Ricardo Patlán <l.papr041025@itses.edu.mx>
2025-04-08 09:55:08 -0600

    Este es el programa concluido en clase Implementamos los metodos TimerTask y Executor

---
+++ src/main/java/Executor/EjemploExecutor.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Executor;
+
+import java.util.concurrent.Executor;
+
+/**
+ *
+ * @author ricar
+ */
+public class EjemploExecutor {
+    public static void main(String[] args) {
+        
+    }
+    
+}
